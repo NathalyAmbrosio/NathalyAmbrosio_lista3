77e50e5713eb300c79318863b72c284b381ef742
NathalyAmbrosio <131813980+NathalyAmbrosio@users.noreply.github.com>
2023-05-24 14:45:00 -0300

    Update NathalyAraujo_exercicio01.java
    add

---
+++ NathalyAraujo_exercicio01.java
@@ -0,0 +1,13 @@
+/* algoritmo que mostra todos números entre 0 e 1000
+Nathaly 
+24/05/2023
+*/
+public class NathalyAraujo_exercicio01{
+public static void main (String args []){
+
+    for (int n = 0; n <= 1000; n++) {
+        System.out.println(n);
+    }
+}
+    
+}
