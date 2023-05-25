21fa36d028cfa8e833b00a47163b0c75e88e3649
NathalyAmbrosio <131813980+NathalyAmbrosio@users.noreply.github.com>
2023-05-25 14:29:42 -0300

    Update NathalyAraujo_exercicio04.java
    add

---
+++ NathalyAraujo_exercicio04.java
@@ -0,0 +1,14 @@
+/* algoritmo que exibi todos os numeros impares de 0 e 1000
+Nathaly 
+24/05/2023
+*/
+public class NathalyAraujo_exercicio04{
+public static void main (String args []){
+     int imp = 1;
+    while (imp <= 1000) {
+        System.out.println(imp);
+        imp += 2;
+    }
+}
+    
+}
